62a58f0cda7580f3fa0dadaad6966784974c70e2
SAKSHAMGARG21 <sakshamgarggarg83@gmail.com>
2024-12-19 13:03:58 +0530

    become instructor botn change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
         {/* Section1 */}
         <div className='relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center text-white justify-between'>
             {/* Top Button */}   
-            <Link to={"/signup"}>
+            {/* <Link to={"/signup"}>
                 <div className=' group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200
                 transition-all duration-200 drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] hover:scale-95 w-fit hover:drop-shadow-none'>
                     <div className='flex flex-row items-center gap-2 rounded-full px-10 py-[5px]
@@ -26,7 +26,7 @@ const Home = () => {
                         <FaArrowRight />
                     </div>
                 </div>
-            </Link>
+            </Link> */}
 
             {/* Heading */}
             <div className='text-center text-4xl font-semibold mt-7'>
